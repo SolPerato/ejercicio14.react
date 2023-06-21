88546381f86ff04ab71bd3a952a82dbcd74f1c9d
Sol Perato <solperato27@gmail.com>
2023-06-21 20:59:26 -0300

    cree la carpeta de recetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./components/common/menu";
+import Footer from "./components/common/Footer";
+import Inicio from "./components/views/Inicio";
 
 function App() {
-
-
   return (
- <div></div>
-  )
+    <>
+      <Menu></Menu>
+      {/* <Error404></Error404> */}
+      <Inicio></Inicio>
+      {/* <DetalleProducto></DetalleProducto> */}
+      {/* <CrearProducto></CrearProducto> */}
+      {/* <EditarProducto></EditarProducto> */}
+      {/*<Administrador></Administrador>*/}
+      <Footer></Footer>
+    </>
+  );
 }
 
-export default App
+export default App;
